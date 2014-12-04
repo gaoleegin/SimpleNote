281d44822b7e9d803fa71802ea21d6dba813aa03
JasonWong <jasonwong@hzclever.com>
2014-12-04 21:31:28 +0800

    Initial Commit

---
+++ SimpleNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleNote
+//
+//  Created by Jason on 14/12/4.
+//  Copyright (c) 2014年 Jason’s Application House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleNote/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleNote
+//
+//  Created by Jason on 14/12/4.
+//  Copyright (c) 2014年 Jason’s Application House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
