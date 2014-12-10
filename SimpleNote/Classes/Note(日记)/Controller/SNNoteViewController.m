c2cd3793c4878bf49a29bdd7d874d553758d3ff5
JasonWong <jasonwong@hzclever.com>
2014-12-10 19:57:55 +0800

    实现阴影渐变动画效果 放弃实现scrollView滚动到page边界时, 禁止滚动 放弃实现竖直方向滑动时, 同时实现水平滑动 放弃按钮监听模拟翻页效果

--- SimpleNote/Classes/Note(日记)/Controller/SNNoteViewController.m
+++ SimpleNote/Classes/Note(日记)/Controller/SNNoteViewController.m
@@ -13,6 +13,10 @@
 #import "Common.h"
 #import "SNNoteModel.h"
 
+
+#define SNSHADOW_ALPHA 0.6 //页尾阴影透明度
+#define SNSHADOW_ANIMATION_RANGE 0.1 //页尾阴影动画范围
+
 @interface SNNoteViewController ()<UIScrollViewDelegate>
 /**
  *  返回日记列表控制器
@@ -67,16 +71,12 @@
  */
 @property (weak, nonatomic) IBOutlet UIScrollView *thirdScrollView;
 /**
- *  当前是否翻向下一页
+ *  上一页的页尾阴影
  */
-@property (nonatomic, assign, getter=isTurnNext) BOOL turnNext;
-/**
- *  当前是否翻回上一页
- */
-@property (nonatomic, assign, getter=isTurnPre) BOOL turnPre;
-
 @property (weak, nonatomic) IBOutlet UIImageView *firstNoteShadow;
-
+/**
+ *  当前页的页尾阴影
+ */
 @property (weak, nonatomic) IBOutlet UIImageView *secondNoteShadow;
 
 @end
@@ -88,7 +88,6 @@
     [super viewDidLoad];
     
     [self addData];
-    
 }
 
 - (void)viewDidLayoutSubviews {
@@ -151,8 +150,7 @@
 }
 
 - (IBAction)preNote {
-    self.index == (self.notes.count - 1) ? [self clickToTurnPage:1] : [self clickToTurnPage:0];
-    NSLog(@"%d", self.index);
+    self.index == self.notes.count - 1 ? [self clickToTurnPage:1] : [self clickToTurnPage:0];
 }
 
 - (IBAction)nextNote {
@@ -161,19 +159,15 @@
 
 #pragma mark <监听事件公共方法>
 - (void)clickToTurnPage:(int)coefficient {
-    [UIView animateWithDuration:0.3 animations:^{
-        self.scrollView.contentOffset = CGPointMake(SCScreenWidth * coefficient, 0);
-    }];
-    [self scrollViewDidEndDecelerating:self.scrollView];
-
+    
+    self.scrollView.contentOffset = CGPointMake(SCScreenWidth * coefficient, 0);
+    
+    [self loopDisplay:coefficient];
 }
 
 #pragma mark - 代理方法
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     
-//    self.turnNext = NO;
-//    self.turnPre = NO;
-
     CGFloat offSetH = self.scrollView.contentOffset.x; // x轴偏移量
     int pageState = offSetH / SCScreenWidth; // 翻页状态下标: 0 or 1 or 2
     
@@ -184,35 +178,29 @@
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     
     CGFloat offsetH = self.scrollView.contentOffset.x - SCScreenWidth;
+
+    if (offsetH < 0) {
+        self.secondNoteLeadingCons.constant = offsetH * 0.5;
+    } else if (offsetH > 0) {
+        self.thirdNoteLeadingCons.constant = - (SCScreenWidth * 0.5) + offsetH * 0.5;
+    }
     
-//    if (offsetH < 0 && self.isTurnNext == NO) {
-//        self.turnPre = YES;
-//        self.secondNoteLeadingCons.constant = offsetH * 0.5;
-//    } else if (offsetH < 0 && self.isTurnNext == YES) {
-//        self.scrollView.contentOffset = CGPointMake(SCScreenWidth, 0);
-//    } else if (offsetH > 0 && self.isTurnPre == NO) {
-//        self.turnNext = YES;
-//        self.thirdNoteLeadingCons.constant = -(SCScreenWidth * 0.5) + offsetH * 0.5;
-//    } else if (offsetH > 0 && self.isTurnPre == YES) {
-//        self.scrollView.contentOffset = CGPointMake(SCScreenWidth, 0);
-//    }
-//    NSLog(@"isTurnPre = %d---isTurnNext = %d",self.isTurnPre, self.isTurnNext);
-    
-    if (offsetH > 0) {
+    if (offsetH > 0) { // 下一页
         self.thirdNoteLeadingCons.constant = -(SCScreenWidth * 0.5) + offsetH * 0.5;
-    } else if (offsetH < 0) {
+    } else if (offsetH < 0) { // 上一页
         self.secondNoteLeadingCons.constant = offsetH * 0.5;
     }
     
-//    NSLog(@"%f", offsetH);
+    // 阴影逻辑实现
+    CGFloat alpha_st = (- offsetH) / (SCScreenWidth * SNSHADOW_ANIMATION_RANGE);
+    if (alpha_st > SNSHADOW_ALPHA) alpha_st = SNSHADOW_ALPHA;
+    self.firstNoteShadow.alpha = alpha_st;
+    
+    CGFloat alpha_nd = (SCScreenWidth - offsetH) / (SCScreenWidth * SNSHADOW_ANIMATION_RANGE);
+    if (alpha_nd > SNSHADOW_ALPHA) alpha_nd = SNSHADOW_ALPHA;
+    self.secondNoteShadow.alpha = alpha_nd;
 }
 
-//- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView {
-//    [UIView animateWithDuration:0.5 animations:^{
-//        self.secondNoteShadow.hidden = YES;
-//    }];
-//    NSLog(@"ok");
-//}
 #pragma mark <更新数据>
 - (void)loopDisplay:(int)pageState {
     // 从第一篇进来后, 翻至第二页时, 模型下标加一 ,不更新数据
@@ -238,7 +226,6 @@
         self.firstNoteView.note = self.notes[self.index - 1];
         self.secondNoteView.note = self.notes[self.index];
         self.thirdNoteView.note = self.notes[self.index + 1];
-        // 复位初始约束
     }
 }
 
